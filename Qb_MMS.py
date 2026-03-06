5c510ee2136fc8a78b70e5fbe56ef55abe99c4e5
LaureJD <laure_jd22@hotmail.com>
2026-03-06 14:27:36 -0500

    correction graphique

--- Qb_MMS.py
+++ Qb_MMS.py
@@ -53,90 +53,57 @@ C_mms = sp.lambdify((r, t, R_sym), C_sym, 'numpy')
 f_mms = sp.lambdify((r, t, Deff, k, R_sym), f_sym, 'numpy')
 g_dir = sp.lambdify((t, R_sym), g_sym, 'numpy')
 
-# ----------------------------
-# Solveur instationnaire (BE + DF2 + Gear)
-# ----------------------------
-def resout_mms(n, deff, k, r_max, dt, t_final):
-    """
-    Résout:
-        C_t = Deff*(C'' + (1/r)C') - k*C + f_MMS
-    CI: C(r,0) = C_MMS(r,0) = 0
-    CL: centre -> Gear ; bord -> Dirichlet g(t)=0
-    """
-    n = int(n)
-    dr = r_max / (n - 1)
-    r = np.linspace(0.0, r_max, n)
-
-    Nt = int(np.round(t_final / dt))
-    t = np.linspace(0.0, Nt*dt, Nt+1)
-
-    # CI = C_MMS(r,0) = 0  (car sin(0)=0)
-    Cn = np.zeros(n, dtype=float)
-    C  = np.zeros((Nt+1, n), dtype=float)
-    C[0, :] = Cn
-
-    A = np.zeros((n, n), dtype=float)
-    b = np.zeros(n, dtype=float)
-
-    for nstep in range(Nt):
-        tn1 = t[nstep+1]
-        A.fill(0.0); b.fill(0.0)
-
-        # i=0 : Gear (Neumann)  ->  (-3 C0 + 4 C1 - C2) / (2 dr) = 0
-        A[0,0] = -3.0
-        A[0,1] =  4.0
-        A[0,2] = -1.0
-        b[0]   =  0.0
-
-        # i=1..N-2 : BE + DF2
-        for i in range(1, n-1):
-            ri = r[i]
-            aW = -deff / dr**2 + deff / (2.0 * ri * dr)
-            aP =  (1.0 / dt) + k + 2.0 * deff / dr**2
-            aE = -deff / dr**2 - deff / (2.0 * ri * dr)
-
-            A[i,i-1] = aW
-            A[i,i]   = aP
-            A[i,i+1] = aE
-
-            # RHS: C^n/dt + f^{n+1}
-            b[i] = Cn[i]/dt + f_mms(ri, tn1, deff, k, r_max)
-
-        # i=N-1 : Dirichlet g(t^{n+1}) = 0
-        A[n-1,:]   = 0.0
-        A[n-1,n-1] = 1.0
-        b[n-1]     = 0.0
-
-        Cnp1 = np.linalg.solve(A, b)
-        C[nstep+1,:] = Cnp1
-        Cn = Cnp1
-
-    return r, t, C
-
-
+print(C_mms)
+print(f_mms)
+print(g_dir)
 # ----------------------------
 # MAIN — Graphiques
 # ----------------------------
 if __name__ == "__main__":
 
-    
+    # Paramètres de résolution
     N       = 101
     DT      = 5e-3
-    T_FINAL = 2.0
-    r_mms, t_mms, C_mms = resout_mms(N, DEFF, K, R, DT, T_FINAL)
-    # print(r_mms)
+    T_FINAL = 20.0
+    # Maillage spatial
+    n = int(N)
+    dr = R / (n - 1)
+    r = np.linspace(0.0, R, n)
 
+    # Maillage temporel
+    Nt = int(np.round(T_FINAL / DT))
+    t = np.linspace(0.0, Nt * DT, Nt + 1)
+    
+    #Tracer MMS
+    C = C_mms(r[:, None], t[None, :], R)
     r_values = [0.0, 0.25*R, 0.5*R, 0.75*R, 0.9*R] 
     plt.figure(figsize=(6.6, 4.6))
     for rv in r_values:
         i = np.argmin(np.abs(r - rv))
-        print(i)
-        label = fr"r = {i:.3f} m"
-        plt.plot(t_mms, C_mms[:, i], lw=2, label=label)
+        label = fr"r = {r[i]:.3f} m"
+        plt.plot(t, C[i, :], lw=2, label=label)
 
     plt.xlabel('t (s)')
-    plt.ylabel('C (mol/m³)')
-    plt.title('Séries temporelles C(t) à plusieurs rayons')
+    plt.ylabel('C_MMS (mol/m³)')
+    plt.title('Séries temporelles C_MMS(t) à plusieurs rayons')
+    plt.grid(True, alpha=0.3)
+    plt.legend()
+    plt.tight_layout()
+    #plt.show()
+
+    #Tracer terme source f_MMS
+    F = f_mms(r[:, None], t[None, :], DEFF, K, R)
+    
+    r_values = [0.0, 0.25*R, 0.5*R, 0.75*R, 0.9*R] 
+    plt.figure(figsize=(6.6, 4.6))
+    for rv in r_values:
+        i = np.argmin(np.abs(r - rv))
+        label = fr"r = {r[i]:.3f} m"
+        plt.plot(t, F[i, :], lw=2, label=label)
+
+    plt.xlabel('t (s)')
+    plt.ylabel('f_MMS (mol/m³/s)')
+    plt.title('Séries temporelles f_MMS(t) à plusieurs rayons')
     plt.grid(True, alpha=0.3)
     plt.legend()
     plt.tight_layout()
